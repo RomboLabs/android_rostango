2df93dc335e66958947fa922531863fde49fd8fe
rai <raiv@u.washington.edu>
2015-07-01 14:50:56 -0700

    scripts updated

--- scripts/tango_listener.py
+++ scripts/tango_listener.py
@@ -13,13 +13,21 @@ import message_filters
 
 from geometry_msgs.msg import TransformStamped
 from tango_msgs.msg import TangoPoseDataMsg
+tango_counter=0;
+vicon_counter=0;
 
-def callback(tango_pose):
-    rospy.loginfo("callback");
-    rospy.loginfo(tango_pose.header.stamp);
+def callback(tango_pose_start_device):
+    #rospy.loginfo("callback");
+    
+    tango_file=open('tango_pose','a')
 
+    tango_file.write(str(tango_pose_start_device.status_code)+','+ str(tango_pose_start_device.header.stamp.secs)+ ','+str(tango_pose_start_device.header.stamp.nsecs)+ ','
+        + str(tango_pose_start_device.translation[0])+ ','+ str(tango_pose_start_device.translation[1])+ ','+ str(tango_pose_start_device.translation[2])+ ','
+        +str(tango_pose_start_device.orientation[0])+','+str(tango_pose_start_device.orientation[1])+','+str(tango_pose_start_device.orientation[2])+','+str(tango_pose_start_device.orientation[3])+'\n') 
+    print('pose_time = '+str(tango_pose_start_device.timestamp)+ ','+ str(tango_pose_start_device.translation)+ ','+str(tango_pose_start_device.orientation)+'\n') 
+    
+    tango_file.close();
 
-    print('pose_time = '+str(tango_pose.timestamp)+ ','+ str(tango_pose.translation)+ ','+str(tango_pose.orientation)+'\n') 
 
 def vicon_callback(vicon_pose):
     rospy.loginfo("vicon_callback");
@@ -31,15 +39,27 @@ def vicon_callback(vicon_pose):
     
 def sync_callback( tango_pose_start_device,  vicon_pose):
      rospy.loginfo("synced");
+     global tango_counter;
+     global vicon_counter;
+     tango_file=open('tango_pose','a')
+     vicon_file =open('vicon_pose','a')
 
-     print("tango: " +str(tango_pose_start_device.header.stamp)  )
-     print("vicon: " + str(vicon_pose.header.stamp))
-     f=open('tango_vicon_pose','a')
+     
+     tango_file.write(str(tango_pose_start_device.status_code)+','+ str(tango_pose_start_device.header.stamp.secs)+ ','+str(tango_pose_start_device.header.stamp.nsecs)+ ','
+        +str(tango_pose_start_device.translation[0])+ ','+ str(tango_pose_start_device.translation[1])+ ','+ str(tango_pose_start_device.translation[2])+ ','
+        +str(tango_pose_start_device.orientation[0])+','+str(tango_pose_start_device.orientation[1])+','+str(tango_pose_start_device.orientation[2])+','+str(tango_pose_start_device.orientation[3])+','
+        +str(tango_counter)+'\n') 
 
-     f.write('pose_time = '+str(tango_pose_start_device.header.stamp)+ ','+ str(tango_pose_start_device.translation)+ ','+str(tango_pose_start_device.orientation)+'\n') 	 
-     f.write('vicon_time = '+str(vicon_pose.header.stamp)+ ','+ str(vicon_pose.transform.translation)+ ','+str(vicon_pose.transform.rotation)+'\n')      
+     vicon_file.write(str(vicon_pose.header.stamp.secs)+ ','+ str(vicon_pose.header.stamp.nsecs)+ ','
+        +str(vicon_pose.transform.translation.x)+ ','+str(vicon_pose.transform.translation.y)+ ','+str(vicon_pose.transform.translation.z)+ ','
+        +str(vicon_pose.transform.rotation.x)+ ','+str(vicon_pose.transform.rotation.y)+ ','+str(vicon_pose.transform.rotation.z)+ ','+str(vicon_pose.transform.rotation.w)+ ','
+        +str(vicon_counter)+'\n')  
+
+     tango_counter +=1;
+     vicon_counter +=1;    
 	
-     f.close();
+     vicon_file.close();
+     tango_file.close();
      
     
 def listener():
@@ -48,14 +68,12 @@ def listener():
 
     #rospy.Subscriber('/tango_pose_start_device', TangoPoseDataMsg, callback)
     #rospy.Subscriber('/vicon/tangoJune29/mainBody', TransformStamped, vicon_callback)
+    
     rospy.loginfo("listening");
 
     tango_sub= message_filters.Subscriber('/tango_pose_start_device', TangoPoseDataMsg) #Start of service to device
-
     vicon_sub= message_filters.Subscriber('/vicon/tangoJune29/mainBody',TransformStamped )  # Vicon pose est 
-    
     ts = message_filters.ApproximateTimeSynchronizer([tango_sub,vicon_sub], 10,5)
-    
     ts.registerCallback(sync_callback)
     
     

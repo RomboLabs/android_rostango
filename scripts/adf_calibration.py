eee7791ae8e03e3906f832e8dfb3dc17e04a26ab
Vijeth Rai <raiv@u.washington.edu>
2015-08-02 17:53:27 -0700

    updated calib and terminals file

--- scripts/adf_calibration.py
+++ scripts/adf_calibration.py
@@ -9,7 +9,7 @@
 
 import rospy
 import message_filters
-
+import os, datetime
 
 from geometry_msgs.msg import TransformStamped
 from tango_msgs.msg import TangoPoseDataMsg
@@ -18,13 +18,20 @@ vicon_counter=0;
 calib_file='vicon_adf_calibration';
 vicon_pose_topic='vicon/TangoJuly24/mainBody';
 
+mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M'))
 
 def vicon_callback(vicon_pose):
     
     global vicon_counter;
     global calib_file;
-
-    vicon_adf_calibration_file=open(calib_file,'w')
+    global mydir
+    
+    x_tolerance = 0.1;
+    y_tolerance = 0.1;
+    z_tolerance = 0.4;	
+    
+    vicon_adf_calibration_file=open(os.path.join(mydir,calib_file),'a')
+              
     vicon_adf_calibration_file.write(str(vicon_pose.header.stamp.secs)+ ','+ str(vicon_pose.header.stamp.nsecs)+ ','
        +str(vicon_pose.transform.translation.x)+ ','+str(vicon_pose.transform.translation.y)+ ','+str(vicon_pose.transform.translation.z)+ ','
        +str(vicon_pose.transform.rotation.x)+ ','+str(vicon_pose.transform.rotation.y)+ ','+str(vicon_pose.transform.rotation.z)+ ','+str(vicon_pose.transform.rotation.w)+ ','
@@ -32,12 +39,23 @@ def vicon_callback(vicon_pose):
 
     vicon_counter +=1;    
      
-
+    
+    if (vicon_pose.transform.translation.x < x_tolerance and vicon_pose.transform.translation.y < y_tolerance and vicon_pose.transform.translation.z < z_tolerance):
+     rospy.loginfo("Device within tolerance to vicon origin... start ADF learning ");
+     
     vicon_adf_calibration_file.close();
 
 
     
 def listener():
+
+    global mydir;
+   
+    try:
+        os.makedirs(mydir)
+    except OSError, e:
+        if e.errno != 17:
+            raise 
 
     rospy.init_node('calibration', anonymous=True)
 

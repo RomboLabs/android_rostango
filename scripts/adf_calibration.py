ec634c22a08b6be370cac58c2f48ae67c3297d77
Vijeth Rai <raiv@u.washington.edu>
2015-08-04 10:59:21 -0700

    adf calib

--- scripts/adf_calibration.py
+++ scripts/adf_calibration.py
@@ -10,9 +10,13 @@
 import rospy
 import message_filters
 import os, datetime
+import sys
+filename= []
 
 from geometry_msgs.msg import TransformStamped
+from tf.transformations import euler_from_quaternion
 from tango_msgs.msg import TangoPoseDataMsg
+
 tango_counter=0;
 vicon_counter=0;
 calib_file='vicon_adf_calibration';
@@ -32,34 +36,65 @@ def vicon_callback(vicon_pose):
     
     vicon_adf_calibration_file=open(os.path.join(mydir,calib_file),'a')
               
-    vicon_adf_calibration_file.write(str(vicon_pose.header.stamp.secs)+ ','+ str(vicon_pose.header.stamp.nsecs)+ ','
-       +str(vicon_pose.transform.translation.x)+ ','+str(vicon_pose.transform.translation.y)+ ','+str(vicon_pose.transform.translation.z)+ ','
+    vicon_adf_calibration_file.write(str(vicon_pose.transform.translation.x)+ ','+str(vicon_pose.transform.translation.y)+ ','+str(vicon_pose.transform.translation.z)+ ','
        +str(vicon_pose.transform.rotation.x)+ ','+str(vicon_pose.transform.rotation.y)+ ','+str(vicon_pose.transform.rotation.z)+ ','+str(vicon_pose.transform.rotation.w)+ ','
        +str(vicon_counter)+'\n') 
 
     vicon_counter +=1;    
      
+    quaternion = (
+    vicon_pose.transform.rotation.x,
+    vicon_pose.transform.rotation.y,
+    vicon_pose.transform.rotation.z,
+    vicon_pose.transform.rotation.w);
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    roll = euler[0]
+    pitch = euler[1]
+    yaw = euler[2]
+    
+    rospy.loginfo("Vicon: Roll:%f,pitch: %f ,yaw:%f",roll,pitch,yaw);
     
     if (vicon_pose.transform.translation.x < x_tolerance and vicon_pose.transform.translation.y < y_tolerance and vicon_pose.transform.translation.z < z_tolerance):
      rospy.loginfo("Device within tolerance to vicon origin... start ADF learning ");
      
     vicon_adf_calibration_file.close();
 
-
+def tango_callback(tango_pose_start_device):
+    rospy.loginfo("tango callback");
+    quaternion_tango = (
+    tango_pose_start_device.orientation[0],
+    tango_pose_start_device.orientation[1],
+    tango_pose_start_device.orientation[2],
+    tango_pose_start_device.orientation[3],);
+    euler = tf.transformations.euler_from_quaternion(quaternion_tango)
+    roll = euler[0]
+    pitch = euler[1]
+    yaw = euler[2]
+    
+    rospy.loginfo("Tango: Roll:%f,pitch: %f ,yaw:%f",roll,pitch,yaw);
+    
     
 def listener():
 
     global mydir;
-   
+    global filename;
+
+  
     try:
         os.makedirs(mydir)
     except OSError, e:
         if e.errno != 17:
             raise 
-
+     
+    if len(sys.argv) == 1:
+      print "No filename chosen... Using default"
+    else:
+      filename = sys.argv[1]
+      
+    
     rospy.init_node('calibration', anonymous=True)
 
-    #rospy.Subscriber('/tango_pose_start_device', TangoPoseDataMsg, callback)
+    rospy.Subscriber('/tango_pose_start_device', TangoPoseDataMsg, tango_callback)
     rospy.Subscriber(vicon_pose_topic, TransformStamped, vicon_callback)
     
     rospy.loginfo("reading vicon pose...start adf recording");
@@ -68,3 +103,4 @@ def listener():
 
 if __name__ == '__main__':
     listener()
+
